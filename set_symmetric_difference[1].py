dc9f83793eaf51f3e23abcfa541b29579ba3a947
swethamani25 <92358649+swethamani25@users.noreply.github.com>
2021-10-15 11:52:18 +0530

    Add files via upload

---
+++ set_symmetric_difference[1].py
@@ -0,0 +1,12 @@
+M = int(input())
+mset = set(map(int, input().split()))
+N = int(input())
+nset = set(map(int, input().split()))
+
+mdef = mset.difference(nset)
+ndef = nset.difference(mset)
+
+output = mdef.union(ndef)
+
+for i in sorted(list(output)):
+    print(i)
